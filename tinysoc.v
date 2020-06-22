7e4f21cc6250528404eb0cceb7b024b503bb8661
Joshua Lloyd <j.nevercast@gmail.com>
2020-06-22 16:28:52 +1200

    Add hardware module to connect tinysoc to TinyFPGA BX Make hardware module toplevel Remove the proginfo script in favour of creating own script

--- tinysoc.v
+++ tinysoc.v
@@ -12,5 +12,34 @@ module tinysoc
          count <= 0;
       end
    end
+endmodule
 
+// Connects tinysoc to the TinyFPGA hardware
+module hardware
+   (
+      // 16MHz clock
+      input CLK,
+      // User LED
+      output LED,
+      
+      // USB Interface
+      output USBPU,
+      output USBP,
+      output USBN,
+
+      );
+
+   // Disable USB interface
+   assign USBPU = 1'b1;
+   assign USBP = 1'b0;
+   assign USBN = 1'b0;
+
+   // Parameter comes from tinysoc.core
+   parameter clk_freq_hz = 16_000_000;
+
+   tinysoc
+     #(.clk_freq_hz (clk_freq_hz))
+   tinyfpga
+     (.clk (CLK),
+      .q   (LED));
 endmodule
