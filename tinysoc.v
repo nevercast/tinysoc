71fb67eae60833cf3ed46e35faf14983fc3351d6
Joshua Lloyd <j.nevercast@gmail.com>
2020-06-22 21:35:55 +1200

    BROKEN: Add usb_uart module to top module

--- tinysoc.v
+++ tinysoc.v
@@ -27,17 +27,62 @@ module hardware
       output USBP,
       output USBN);
 
-   // Disable USB interface
-   assign USBPU = 1'b1;
-   assign USBP = 1'b0;
-   assign USBN = 1'b0;
-
    // Parameter comes from tinysoc.core
    parameter clk_freq_hz = 16_000_000;
 
+   // Add tinysoc module (which just blinks the LED for now)
    tinysoc
      #(.clk_freq_hz (clk_freq_hz))
    tinyfpga
      (.clk (CLK),
       .q   (LED));
+   
+   wire clk_48mhz;
+
+   wire clk_locked;
+
+   // Use an icepll generated pll
+   pll pll48( .clock_in(CLK), .clock_out(clk_48mhz), .locked( clk_locked ) );
+
+   // Generate reset signal
+   reg [5:0] reset_cnt = 0;
+   wire reset = ~reset_cnt[5];
+   always @(posedge clk_48mhz)
+      if ( clk_locked )
+         reset_cnt <= reset_cnt + reset;
+
+   // uart pipeline in
+   wire [7:0] uart_in_data;
+   wire       uart_in_valid;
+   wire       uart_in_ready;
+   
+   wire usb_p_tx;
+   wire usb_n_tx;
+   wire usb_p_rx;
+   wire usb_n_rx;
+   wire usb_tx_en;
+
+   // usb uart - this instanciates the entire USB device.
+   usb_uart uart (
+      .clk_48mhz  (clk_48mhz),
+      .reset      (reset),
+
+      // pins
+      .USBP( USBP ),
+      .USBN( USBN ),
+
+      // uart pipeline in
+      .uart_in_data( uart_in_data ),
+      .uart_in_valid( uart_in_valid ),
+      .uart_in_ready( uart_in_ready ),
+
+      .uart_out_data( uart_in_data ),
+      .uart_out_valid( uart_in_valid ),
+      .uart_out_ready( uart_in_ready  )
+
+      //.debug( debug )
+   );
+
+   // USB Host Detect Pull Up
+   assign USBPU = 1'b1;
 endmodule
