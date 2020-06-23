6123c9de91ee4f97f66ed815a3f99c389176a0e7
Joshua Lloyd <j.nevercast@gmail.com>
2020-06-23 15:26:13 +1200

    Experiment with getting GPIO working

--- tinysoc.v
+++ tinysoc.v
@@ -64,13 +64,23 @@ module hardware
    // Only begins to reset after the
    // 48MHz PLL is locked.
    ///////////////////////////////////
-   reg [5:0] reset_cnt = 0;
-   wire resetn = &reset_cnt;
-   wire reset = ~reset_cnt[5];
+   reg [24:0] reset_cnt = 0;
+   reg was_in_reset = 1;
+   wire reset = ~reset_cnt[24];
+   wire resetn = ~reset;
+   wire BOOT_LED;
 
    always @(posedge CLK) begin
-      if ( clk_locked )
-         reset_cnt <= reset_cnt + !resetn;
+      if ( clk_locked ) begin
+         reset_cnt <= reset_cnt + reset;
+         if (reset) begin 
+            BOOT_LED <= reset_cnt[19];
+         end
+         if (!reset && was_in_reset) begin 
+            was_in_reset <= 1;
+            BOOT_LED <= 1;
+         end
+      end
    end
 
    // uart pipeline in
@@ -105,9 +115,6 @@ module hardware
       //.debug( debug )
    );
 
-   // Clock wire for picosoc
-   wire clk = clk_16mhz;
-
    ///////////////////////////////////
    // SPI Flash Interface
    ///////////////////////////////////
@@ -136,13 +143,24 @@ module hardware
    wire [31:0] iomem_wdata;
    reg  [31:0] iomem_rdata;
 
-   reg [31:0] gpio;
-   assign LED = gpio[0];
+   reg [24:0] led_pulse_ctr = 0;
 
-   always @(posedge clk) begin
-      if (!resetn) begin
+   reg [31:0] gpio = 0;
+   wire USER_LED = gpio[0];
+   wire USER_LED_ENABLE = gpio[1];
+   wire PULSE_LED = led_pulse_ctr[24];
+   wire RUNTIME_LED = USER_LED_ENABLE ? USER_LED : PULSE_LED;
+   assign LED = reset ? BOOT_LED : RUNTIME_LED;
+
+   always @(posedge CLK) begin
+      if (reset) begin
          gpio <= 0;
       end else begin
+
+         if (!USER_LED_ENABLE) begin 
+            led_pulse_ctr <= led_pulse_ctr + 1;
+         end
+
          iomem_ready <= 0;
 
          ///////////////////////////
@@ -168,44 +186,44 @@ module hardware
       end
    end
 
-   picosoc #(
-      .PROGADDR_RESET(32'h0005_0000), // beginning of user space in SPI flash
-      .PROGADDR_IRQ(32'h0005_0010),
-      .MEM_WORDS(2048)                // use 2KBytes of block RAM by default
-   ) soc (
-      .clk          (clk         ),
-      .resetn       (resetn      ),
+   // picosoc #(
+   //    .PROGADDR_RESET(32'h0005_0000), // beginning of user space in SPI flash
+   //    .PROGADDR_IRQ(32'h0005_0010),
+   //    .MEM_WORDS(2048)                // use 2KBytes of block RAM by default
+   // ) soc (
+   //    .clk          (CLK         ),
+   //    .resetn       (resetn      ),
 
-      .ser_tx       (PIN_TX       ),
-      .ser_rx       (PIN_RX       ),
+   //    .ser_tx       (PIN_TX       ),
+   //    .ser_rx       (PIN_RX       ),
 
-      .flash_csb    (SPI_SS   ),
-      .flash_clk    (SPI_CLK   ),
+   //    .flash_csb    (SPI_SS   ),
+   //    .flash_clk    (SPI_CLK   ),
 
-      .flash_io0_oe (flash_io0_oe),
-      .flash_io1_oe (flash_io1_oe),
-      .flash_io2_oe (flash_io2_oe),
-      .flash_io3_oe (flash_io3_oe),
+   //    .flash_io0_oe (flash_io0_oe),
+   //    .flash_io1_oe (flash_io1_oe),
+   //    .flash_io2_oe (flash_io2_oe),
+   //    .flash_io3_oe (flash_io3_oe),
 
-      .flash_io0_do (flash_io0_do),
-      .flash_io1_do (flash_io1_do),
-      .flash_io2_do (flash_io2_do),
-      .flash_io3_do (flash_io3_do),
+   //    .flash_io0_do (flash_io0_do),
+   //    .flash_io1_do (flash_io1_do),
+   //    .flash_io2_do (flash_io2_do),
+   //    .flash_io3_do (flash_io3_do),
 
-      .flash_io0_di (flash_io0_di),
-      .flash_io1_di (flash_io1_di),
-      .flash_io2_di (flash_io2_di),
-      .flash_io3_di (flash_io3_di),
+   //    .flash_io0_di (flash_io0_di),
+   //    .flash_io1_di (flash_io1_di),
+   //    .flash_io2_di (flash_io2_di),
+   //    .flash_io3_di (flash_io3_di),
 
-      .irq_5        (1'b0        ),
-      .irq_6        (1'b0        ),
-      .irq_7        (1'b0        ),
+   //    .irq_5        (1'b0        ),
+   //    .irq_6        (1'b0        ),
+   //    .irq_7        (1'b0        ),
 
-      .iomem_valid  (iomem_valid ),
-      .iomem_ready  (iomem_ready ),
-      .iomem_wstrb  (iomem_wstrb ),
-      .iomem_addr   (iomem_addr  ),
-      .iomem_wdata  (iomem_wdata ),
-      .iomem_rdata  (iomem_rdata )
-   );
+   //    .iomem_valid  (iomem_valid ),
+   //    .iomem_ready  (iomem_ready ),
+   //    .iomem_wstrb  (iomem_wstrb ),
+   //    .iomem_addr   (iomem_addr  ),
+   //    .iomem_wdata  (iomem_wdata ),
+   //    .iomem_rdata  (iomem_rdata )
+   // );
 endmodule
